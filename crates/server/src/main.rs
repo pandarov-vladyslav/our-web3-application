614673693a85858cca6193a4ccb15e7a44b34ca3
pandarov-vladyslav <pandarov.vladyslav.o@chnu.edu.ua>
2025-10-21 18:34:23 +0300

    Додано кнопку для підвантаження crypto-top через htmx та шаблон crypto-top-table.html

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -8,7 +8,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/", get(index))
         .route("/hello-world", get(hello_world))
         .route("/favicon.ico", get(favicon))
-        .nest_service("/static", ServeDir::new("crates/server/static"));
+        .nest_service("/static", ServeDir::new("server/static"))
+        .route("/crypto-top", get(crypto_top));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8081").await?;
     println!("Running on http://0.0.0.0:8081");
@@ -17,7 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn index() -> Result<Html<String>, StatusCode> {
-    let index = fs::read_to_string("crates/server/templates/index.html")
+    let index = fs::read_to_string("server/templates/index.html")
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
@@ -31,4 +32,16 @@ async fn hello_world() -> Html<String> {
 
 async fn favicon() -> axum::response::Redirect {
     axum::response::Redirect::permanent("/static/svg/icon.svg")
+}
+
+async fn crypto_top() -> Result<Html<String>, StatusCode> {
+    let html = r#"
+        <h2>Топ 5 криптовалют</h2>
+        <table>
+            <tr><td>1</td><td>Bitcoin</td><td>60000</td></tr>
+            <tr><td>2</td><td>Ethereum</td><td>4000</td></tr>
+        </table>
+    "#;
+
+    Ok(Html(html.to_string()))
 }
